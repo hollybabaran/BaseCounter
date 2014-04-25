b5aac7b9b22e3a9c3f81e9c1cd492822669a5c69
HBabaran <holly.babaran@oicr.on.ca>
2014-04-25 10:16:28 -0400

    stuff

--- main.pl
+++ main.pl
@@ -3,6 +3,20 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use File::Find;
+use Cwd;
+
+my $record = "test/baseCounter.csv";
+my $date;
+GetOptions ("d=s" => \$date,
+            "o=s" => \$record           
+            )
+    or usage("Incorrect options.");
+my $mtime = (stat $record)[9]; 
+if($date){} #TODO convert date to mtime and overwrite mtime
+
+my $files_ref = findBcls($mtime); #returns a hash of machine + files
+sumAndPrint($files_ref);
 
 sub usage{
     print "perl main.pl [options]\n";
@@ -10,19 +24,20 @@ sub usage{
     print "\t-d <date>; default is timestamp of output file\n"; 
 }
 
-my $record = "baseCounter.csv";
-my $mtime = (stat $record)[9]; 
+sub findBcls{
+    my $date = shift;
+    find(\&printBcls, getcwd);
+}
 
-GetOptions ("d=s" => \$date,
-            "o=s" => \$record           
-            )
-    or usage("Incorrect options.");
-#TODO convert date to mtime and overwrite mtime
+sub printBcls{
+    my $mtime2 = (stat $File::Find::name)[9]; 
+    if($File::Find::name =~ /([Dhim][0-9]+)\[0-9a-zA-z]+\/Data\/Intensities\/BaseCalls\/L[0-9]+\/C[0-9]+\.1\/[a-zA-Z0-9_]+\.bcl/ && $mtime2 gt $mtime){
+        print "$File::Find::name\n";
+    }
+}
+
+sub sumAndPrint{
+    
+}
 
 
-
-#my $mtime1 = (stat "clic_thePLAN.txt")[9]; 
-#my $mtime2 = (stat "blabdsds")[9];
-#
-#print "clic_thePLAN was made at: $mtime1\n";
-#print "blabdsds was made at: $mtime2\n";
